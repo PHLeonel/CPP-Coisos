caef86dbecf6a4de858978edd27d8c1c95565990
PHLeonel <104041093+PHLeonel@users.noreply.github.com>
2026-03-09 15:38:33 -0300

    Add files via upload

---
+++ Princípios.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{	
+	
+	int inteiro = 3; //a; b; res;
+	char letra = 'B'; //único caractere
+	double decimal = 2.333333; //decimal específico
+	float decima = 2.3; //decimal não tão específico 
+	bool verdade = true; //v ou f
+	string bola="."; //escrita
+	
+	// (6 TIPOS)
+	
+	cout << "Escreve ae: " << endl;
+	cin >> bola;
+	cout << "O que tu escreveu foi: " << bola << endl;
+	
+	return 0;	
+	
+}
+
